1de7158cf0f7adfbd30591cc0a4502d42e27789b
Akshita <akshita.g1507@gmail.com>
2023-12-17 23:13:39 +0530

    added questions for round-886 (div-4)

---
+++ codeforces/round 886 (div-4)/qB.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+ 
+#define vi vector<ll>
+#define rep(x, start, end) for(ll x = start; x < end; x++)
+#define rrep(x, start, end) for(ll x = start; x >= end; x--)
+#define py cout << "YES" << endl
+#define pn cout << "NO" << endl
+#define p(x) cout << x << endl
+#define parr(arr) rep(i, 0, arr.size()) cout << arr[i] << " "; cout << endl
+
+//-------------------------- Debug -------------------------/
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x <<" -> "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+ 
+void _print(ll t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(lld t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(ull t) {cerr << t;}
+ 
+void solve()
+{
+    ll n;
+    cin >> n;
+
+    ll ans = INT_MIN;
+    ll ind = 0;
+
+    for(int i=0; i<n; i++)
+    {
+        ll a, b;
+        cin >> a >> b;
+
+
+        if(a <= 10)
+        {
+            if(ans < b)
+            {
+                ans = b;
+                ind = i;
+            }
+        }
+    }
+    p(ind+1);
+}
+ 
+//--------------------------- MAIN -------------------------/
+int main() {
+    ios_base::sync_with_stdio(false); // to make input output faster
+    cin.tie(NULL);
+
+    int t; 
+    cin >> t;
+    while(t--)
+    {
+        solve();
+    }
+    return 0;
+}
