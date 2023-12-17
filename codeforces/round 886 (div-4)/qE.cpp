d86333abcdd51472701b43acdf7a12fbfaed8437
Akshita <akshita.g1507@gmail.com>
2023-12-17 23:14:21 +0530

    added questions for round-886 (div-4)

---
+++ codeforces/round 886 (div-4)/qE.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+ 
+#define vi vector<ll>
+#define rep(x, start, end) for(ll x = start; x < end; x++)
+#define rrep(x, start, end) for(ll x = start; x >= end; x--)
+#define py cout << "YES" << endl
+#define pn cout << "NO" << endl
+#define p(x) cout << x << endl
+#define parr(arr) rep(i, 0, arr.size()) cout << arr[i] << " "; cout << endl
+
+//-------------------------- Debug -------------------------/
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x <<" -> "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+ 
+void _print(ll t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(lld t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(ull t) {cerr << t;}
+ 
+void solve()
+{
+    ll n, c;
+    cin >> n >> c;
+
+    ll sum = 0, sumsq = 0;
+
+    for(int i=0; i<n; i++)
+    {
+        int a;
+        cin >> a;
+
+        sum += a;
+        sumsq += a*a;
+    }
+
+    ll C = (c-sumsq)/4;
+    C = C/n;
+
+    ll num1 = sum/(2*n);
+    ll num2 = num1*num1;
+
+    ll dis = sqrtl(num2 + C);
+    dis -= num1;
+
+    ll ans = dis;
+    p(ans);
+}
+ 
+//--------------------------- MAIN -------------------------/
+int main() {
+    ios_base::sync_with_stdio(false); // to make input output faster
+    cin.tie(NULL);
+
+    int t; 
+    cin >> t;
+    while(t--)
+    {
+        solve();
+    }
+    return 0;
+}
