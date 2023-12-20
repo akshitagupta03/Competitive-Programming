1c5c4cef17c42fdd5626a87f027185dca0ec5ec4
Akshita Gupta <akshita.g1507@gmail.com>
2023-12-20 22:09:33 +0530

    added questions for starters 110 (div-3)

---
+++ Codechef/Starters 110/q4.cpp
@@ -0,0 +1,78 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+ 
+#define vi vector<ll>
+#define rep(x, start, end) for(ll x = start; x < end; x++)
+#define rrep(x, start, end) for(ll x = start; x >= end; x--)
+#define py cout << "YES" << endl
+#define pn cout << "NO" << endl
+#define p(x) cout << x << endl
+#define parr(arr) rep(i, 0, arr.size()) cout << arr[i] << " "; cout << endl
+
+//-------------------------- Debug -------------------------/
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x <<" -> "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+ 
+void _print(ll t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(lld t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(ull t) {cerr << t;}
+ 
+void solve()
+{
+    ll a, b;
+    cin >> a >> b;
+    
+    ll count1 = 0;
+    ll count2 = 0;
+    
+    ll temp1 = a;
+    ll temp2 = b;
+    
+    while(temp1 % temp2 != 0)
+    {
+        count1++;
+        temp1++;
+        temp2--;
+    }
+    
+    while(a % b != 0)
+    {
+        count2++;
+        a--;
+        b++;
+    }
+    
+    if(a == 0)
+    {
+        p(count1);
+        return;
+    }
+    
+    ll ans = min(count1, count2);
+    p(ans);
+}
+ 
+//--------------------------- MAIN -------------------------/
+int main() {
+    ios_base::sync_with_stdio(false); // to make input output faster
+    cin.tie(NULL);
+
+    int t; 
+    cin >> t;
+    while(t--)
+    {
+        solve();
+    }
+    return 0;
+}
