54a9b81d42a8a41794ed822fd0b400c234100299
Akshita Gupta <akshita.g1507@gmail.com>
2023-12-21 15:02:45 +0530

    added questions for starters 113 (div-3)

---
+++ Codechef/Starters 113/q4.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+ 
+#define vi vector<ll>
+#define rep(x, start, end) for(ll x = start; x < end; x++)
+#define rrep(x, start, end) for(ll x = start; x >= end; x--)
+#define py cout << "YES" << endl
+#define pn cout << "NO" << endl
+#define p(x) cout << x << endl
+#define parr(arr) rep(i, 0, arr.size()) cout << arr[i] << " "; cout << endl
+
+//-------------------------- Debug -------------------------/
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x <<" -> "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+ 
+void solve()
+{
+    ll x, y;
+    cin >> x >> y;
+
+    if(y > x)
+    {
+        p(1);
+        return;
+    }
+
+    ll partitions = y+1;
+    ll num = ceil(x*(1.0)/partitions);
+    ll subarraySum = x - 2*y;
+    
+    ll ans = max(num, subarraySum);
+    p(ans);
+}
+ 
+//--------------------------- MAIN -------------------------/
+int main() {
+    ios_base::sync_with_stdio(false); // to make input output faster
+    cin.tie(NULL);
+
+    int t; 
+    cin >> t;
+    while(t--)
+    {
+        solve();
+    }
+    return 0;
+}
