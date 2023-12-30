18c929a00e70b1e92320c4c81f994d04db19108f
Akshita <akshita.g1507@gmail.com>
2023-12-30 15:34:19 +0530

    added questions for starters 114 (div-3)

---
+++ Codechef/Starters 114/q1.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+ 
+#define vi vector<ll>
+#define py cout << "YES" << endl
+#define pn cout << "NO" << endl
+#define p(x) cout << x << endl
+#define parr(arr) rep(i, 0, arr.size()) cout << arr[i] << " "; cout << endl
+
+ 
+void solve()
+{
+    ll x, y, z;
+    cin >> x >> y >> z;
+
+    float dim = x*y;
+    float money = z/2;
+
+    float ans = floor(money/dim);
+    p(ans);
+}
+ 
+//--------------------------- MAIN -------------------------/
+int main() {
+    ios_base::sync_with_stdio(false); // to make input output faster
+    cin.tie(NULL);
+
+    int t; 
+    cin >> t;
+    while(t--)
+    {
+        solve();
+    }
+    return 0;
+}
