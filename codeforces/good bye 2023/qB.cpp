ad5b286922d75680f4b2400bc0f1f50271583593
Akshita <akshita.g1507@gmail.com>
2024-01-01 15:43:39 +0530

    last contest of the year

---
+++ codeforces/good bye 2023/qB.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+ 
+#define vi vector<ll>
+#define py cout << "YES" << endl
+#define pn cout << "NO" << endl
+#define p(x) cout << x << endl
+#define all(x) (x).begin(),(x).end()
+#define parr(arr) rep(i, 0, arr.size()) cout << arr[i] << " "; cout << endl
+
+int gcd(int a, int b) 
+{ 
+    // Find Minimum of a and b 
+    int result = min(a, b); 
+    while (result > 0) { 
+        if (a % result == 0 && b % result == 0) { 
+            break; 
+        } 
+        result--; 
+    } 
+  
+    // Return gcd of a and b 
+    return result; 
+} 
+
+void solve()
+{
+    ll a, b;
+    cin >> a >> b;
+
+    ll lcm = (a*b) / gcd(a, b);
+    
+    if(lcm == b)
+    {
+        ll ans = lcm * (b/a);
+        p(ans);
+    }
+    else
+    {
+        p(lcm);
+    }
+}
+ 
+//--------------------------- MAIN -------------------------/
+int main() {
+    ios_base::sync_with_stdio(false); // to make input output faster
+    cin.tie(NULL);
+
+    int t; 
+    cin >> t;
+    while(t--)
+    {
+        solve();
+    }
+    return 0;
+}
